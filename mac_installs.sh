8395558b3e7b26ee99df47c289636e7cd4c276b3
deybyr647 <deybyr647@gmail.com>
2023-05-04 11:30:36 -0400

    Refactored MacOS Install Script

--- mac_installs.sh
+++ mac_installs.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
 # Install Homebrew if not already installed
+echo -e "\nChecking if Homebrew is already installed..."
 if ! command -v brew &> /dev/null; then
+    echo -e "\nInstalling Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Update Homebrew and formulae
+echo -e "\nUpdating Homebrew & Formulae"
 brew update
 
 # Install Python 3 and PIP 3
+echo -e "\nInstalling Python 3 & PIP 3.."
 brew install python@3
 
 # Upgrade PIP
+echo -e "\nUpdating PIP 3..."
 pip3 install --upgrade pip
 
 # Install Python packages
+echo -e "\nInstalling AutoGPT dependencies..."
 pip3 install streamlit langchain openai wikipedia chromadb tiktoken
 
 # Verify installation
-echo "Python 3, PIP 3, and required packages have been installed."
+echo -e "\nPython 3, PIP 3, and required packages have been installed."
